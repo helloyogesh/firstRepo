d9a1ebc973c3cf7b38fc98cdc98e9a9b5f1788a9
Dev2 <Dev2@gmail.com>
2020-04-21 12:57:17 +0530

    dev 1 modified test.java

--- DemoProj1/src/com/yogesh/test/Test.java
+++ DemoProj1/src/com/yogesh/test/Test.java
@@ -1,5 +1,12 @@
 package com.yogesh.test;
 
-public class Test {
+import javax.swing.text.Utilities;
 
+import com.yogesh.beans.Utility;
+
+public class Test {
+	public static void main(String[] args) {
+		Utility u=new Utility();
+		System.out.println(u.getDate());
+	}
 }
